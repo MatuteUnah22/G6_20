4a60f2c2d04230050d82d5654d57eadaf02941fb
ndoa72 <ndochoa@unah.hn>
2021-11-21 13:40:27 -0600

    Correccion de URL API Articulos

--- Articulos/controller/articulos.php
+++ Articulos/controller/articulos.php
@@ -1,8 +1,8 @@
 <?php 
     header('Content-Type: application/json');
 
-    require_once("../config/conexion.php");
-    require_once("../models/Articulos.php");
+    require_once("../../config/conexion.php");
+    require_once("../../Articulos/models/Articulos.php");
     $articulos = new Articulos();
 
     $body = json_decode(file_get_contents("php://input"), true);
